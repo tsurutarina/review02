859c4cbca04b7a9870cfe556bafbadbe5fc6e107
tsuruta <naritakundono@gmail.com>
2023-01-31 13:43:01 +0000

    [Add]variable

---
+++ exercise/Chapter04.java
@@ -0,0 +1,12 @@
+public class Chapter04 {
+  public static void main(String[] args) {
+    final int LUCKY_NUMBER = 6;
+    String name = "tsuruta";
+    System.out.println(name);
+
+    name = "aiueo";
+    System.out.println(name);
+
+    System.out.println(LUCKY_NUMBER);
+  }
+}
