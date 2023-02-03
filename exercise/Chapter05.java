539cd412b1b8832d1360c03c760b90f93682ff61
tsuruta <naritakundono@gmail.com>
2023-02-03 13:45:31 +0000

    [Add]型

---
+++ exercise/Chapter05.java
@@ -0,0 +1,20 @@
+public class Chapter05 {
+  public static void main(String[] args) {
+    long l = 1234567890L;
+    System.out.println(l);
+    double d = 123.456;
+    System.out.println(d);
+    boolean b = true;
+    System.out.println(b);
+
+    Float F = 0.123f;
+    System.out.println(F);
+    Integer I = 123;
+    System.out.println(I);
+    Character C = 'A';
+    System.out.println(C);
+
+    int[] i = {0,1,2,3,4};
+    System.out.println(i[3]);
+  }
+}
